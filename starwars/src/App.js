1971300413976e07b8f00f6cd8c7d736e5805dec
Judy Ghashim <judyghashim@Judys-MacBook-Pro.local>
2020-04-18 13:58:40 -0500

    working on styling

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,10 +1,73 @@
+// import React, { useState, useEffect } from "react";
+// import "./App.css";
+// import axios from'axios'
+// import Characters from "../src/components/Character";
+// import styled from "styled-components";
+
+// const H = styled.h1`
+// font-size: 4rem;
+//   color:white;
+//   font-family: 'Indie Flower', cursive;
+//   text-align:center;
+// `;
+
+// const App = () => {
+//   // Try to think through what state you'll need for this app before starting. Then build out
+//   // the state properties here.
+
+//   // Fetch characters from the API in an effect hook. Remember, anytime you have a
+//   // side effect in a component, you want to think about which state and/or props it should
+//   // sync up with, if any.
+
+//   const [characters, setCharacters] = useState([]);
+//   // console.log("Characters", characters[0]);
+//   useEffect(() => {
+//     axios
+//       .get('https://rickandmortyapi.com/api/character/')
+//       .then(res=> setCharacters(res.data.results))
+//       .catch(err=>console.log('We having technical issue', err))
+//   },[]);
+
+//   return (
+//     <div className="App">
+//       <H> Characters</H>
+
+//       {/* <div className ='character'>
+//         {characters.map(character => (
+//           <character key={character.id} name={character.name} img={character.image}  />
+//         ))}
+//       </div> */}
+
+//     </div>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import axios from'axios'
-import Characters from "../src/components/Character";
+import axios from "axios";
+import Character from "../src/components/Character";
 import styled from "styled-components";
 
+import Container from "reactstrap/lib/Container";
+
+const H = styled.h1`
+  font-size: 4rem;
+  color: white;
+`;
+
 const App = () => {
+  const [characters, setCharacters] = useState([]);
+  // const [isHappy, setIsHappy]= useState(true);
+  console.log("Characters", characters[0]);
+
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then((res) => setCharacters(res.data.results))
+      .catch((err) => console.log("Nope", err));
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,19 +75,25 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  const [characters, setCharacters] = useState([]);
-  // console.log("Characters", characters[0]);
-  useEffect(() => {
-    axios
-      .get("https://swapi.py4e.com/api/films/1")
-      .then(res=> setCharacters(res.data.results))
-      .catch(err=>console.log('We having technical issue',err))
-  },[]);
-
   return (
+    <Container>
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <H>Characters</H>
+
+      <div className="character-container">
+        {characters.map((character) => (
+          <Character
+            key={character.id}
+            name={character.name}
+            img={character.image}
+            gender={character.gender}
+            status={character.status}
+            species={character.species}
+          />
+        ))}
+      </div>
     </div>
+    </Container>
   );
 };
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,51 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+import { Container, Row, Col } from "reactstrap";
+
+const Card = styled.div`
+  background: #202429;
+  margin: auto;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-item: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  
+`;
+
+const CharacterImg = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: scale;
+  margin: auto;
+`;
+
+const P = styled.p`
+  color: white;
+  display: flex;
+  align-text: left;
+  margin:auto;
+`;
+
+const H = styled.h2`
+  color: white;
+  font-size: 2rem;
+`;
+
+const Character = (props) => {
+  return (
+    <Col sm="6" sm="6" md="4" xl="3">
+      <Card>
+        <H>{props.name}</H>
+        <CharacterImg top width="10%" src={props.img} alt="Characters" />
+        <P>Gender: {props.gender}</P>
+        <P>Status: {props.status}</P>
+        <P>Species: {props.species}</P>
+      </Card>
+    </Col>
+  );
+};
+
+export default Character;
